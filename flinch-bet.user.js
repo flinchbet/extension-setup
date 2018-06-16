59199cd86dba39fb0a731b7b11b63646b17b8bc0
Kevin Upton <kevupton@users.noreply.github.com>
2018-06-16 18:44:46 +1000

    Update flinch-bet.user.js

--- flinch-bet.user.js
+++ flinch-bet.user.js
@@ -1,15 +1,16 @@
 // ==UserScript==
 // @name Flinch Bet
 // @namespace #flinchbet
-// @version 1.0.1
+// @version 1.0.2
 // @description Adds a script to twitch which allows you to bet on live streaming.
-// @copyright 2017+, Flinch Bet <admin@flinchbet.com> (https://github.com/flinchbet), http://flinchbet.com
-// @author flinchbet <dev@flinchbet.com> (https://github.com/flinchbet)
+// @copyright 2017+, Flinch Bet <admin@flinch.bet> (https://github.com/flinchbet), https://flinch.bet
+// @author flinchbet <dev@flinch.bet> (https://github.com/flinchbet)
 // @icon https://raw.githubusercontent.com/flinchbet/extension-setup/master/icon.png
 // @license MIT; http://opensource.org/licenses/MIT
 // @updateURL https://github.com/flinchbet/extension-setup/raw/master/flinch-bet.user.js
-// @homepageURL http://flinchbet.com/
-// @supportURL https://flinchbet.com/admin/support
+// @downloadURL https://github.com/flinchbet/extension-setup/raw/master/flinch-bet.user.js
+// @homepageURL https://flinch.bet/
+// @supportURL https://flinch.bet/f/support
 // @contributionURL https://github.com/flinchbet/extension-setup
 // @grant none
 // @include http://*.twitch.tv/*
